9e8ced67cc2d8a357651f3551b7bd5624455f102
David <daveman@gmail.com>
2023-05-02 15:59:54 -0700

    scratched tsparticles for now, using sparticles-bg

--- section24/smart-brain-front-end/src/App.js
+++ section24/smart-brain-front-end/src/App.js
@@ -4,12 +4,14 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkURL from './components/ImageLinkURL/ImageLinkURL';
 import Rank from './components/Rank/Rank';
+// import FacePattern from './components/FacePattern/FacePattern';
+import ParticlesBg from 'particles-bg';
 
 class App extends Component {
   render () {
     return (
       <div className="App">
-        <h1>Helloooo World!</h1>
+        <ParticlesBg type="circle" bg={true} />
         <Navigation />
         <Logo />
         <Rank />
