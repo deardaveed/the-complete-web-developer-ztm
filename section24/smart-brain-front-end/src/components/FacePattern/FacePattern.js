105d0d296f58c04e754e6855add2d5d2d83a7e83
David <daveman@gmail.com>
2023-05-23 01:39:32 -0700

    clean up code

--- section24/smart-brain-front-end/src/components/FacePattern/FacePattern.js
+++ section24/smart-brain-front-end/src/components/FacePattern/FacePattern.js
@@ -1,9 +1,4 @@
 import React from 'react';
-// const Clarifai = require('clarifai');
-
-// const app = new Clarifai.App({
-//  apiKey: 'a3190f3263024e0db05c173677ebfc85'
-// });
 
 const FacePattern = ({imageUrl}) => {
   return (
