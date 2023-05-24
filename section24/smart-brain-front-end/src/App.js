2ef20341f635e6b389fb694e00a2a61964f31ff8
David <daveman@gmail.com>
2023-05-24 02:09:55 -0700

    updated

--- section24/smart-brain-front-end/src/App.js
+++ section24/smart-brain-front-end/src/App.js
@@ -8,91 +8,91 @@ import FacePattern from './components/FacePattern/FacePattern';
 import ParticlesBg from 'particles-bg';
 
 const returnClarifaiRequestOptions = (imgURL) => {
-    const PAT = 'a7f0fc84f6424161a0a613e8fbafde68';
-    const USER_ID = 't4tjvl7769yx';
-    const APP_ID = 'ztm';
-    const IMAGE_URL = imgURL
+  const PAT = 'a7f0fc84f6424161a0a613e8fbafde68';
+  const USER_ID = 't4tjvl7769yx';
+  const APP_ID = 'ztm';
+  const IMAGE_URL = imgURL;
 
-    const raw = JSON.stringify({
-        "user_app_id": {
-            "user_id": USER_ID,
-            "app_id": APP_ID
-        },
-        "inputs": [{
-            "data": {
-                "image": {
-                    "url": IMAGE_URL
-                }
-            }
-        }]
-    });
+  const raw = JSON.stringify({
+    "user_app_id": {
+      "user_id": USER_ID,
+      "app_id": APP_ID
+    },
+    "inputs": [{
+      "data": {
+        "image": {
+          "url": IMAGE_URL
+        }
+      }
+    }]
+  });
 
-    const requestOptions = {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Authorization': 'Key ' + PAT
-        },
-        body: raw
-    };
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Authorization': 'Key ' + PAT
+    },
+    body: raw
+  };
 
-    return requestOptions;
+  return requestOptions;
 }
 
 class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            input: '',
-            imageUrl: '',
-            box: {}
-        }
+  constructor() {
+    super()
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {}
     }
+  }
 
-    calculateFaceDimensions = (data) => {
-        const clarifaiBounds = data.outputs[0].data.regions[0].region_info.bounding_box
-        const image = document.getElementById('input-image');
-        const width = +image.width;
-        const height = +image.height;
-        
-        return {
-            leftCol: clarifaiBounds.left_col * width,
-            topRow: clarifaiBounds.top_row * height,
-            rightCol: width - (clarifaiBounds.right_col * width),
-            bottomRow: height - (clarifaiBounds.bottom_row * height)
-        }
+  calculateFaceDimensions = (data) => {
+    const clarifaiBounds = data.outputs[0].data.regions[0].region_info.bounding_box
+    const image = document.getElementById('input-image');
+    const width = +image.width;
+    const height = +image.height;
+
+    return {
+      leftCol: clarifaiBounds.left_col * width,
+      topRow: clarifaiBounds.top_row * height,
+      rightCol: width - (clarifaiBounds.right_col * width),
+      bottomRow: height - (clarifaiBounds.bottom_row * height)
     }
+  }
 
-    drawFaceBox = (dimensions) => {
-        console.log(dimensions);
-        this.setState({box: dimensions});
-    }
+  drawFaceBox = (dimensions) => {
+    console.log(dimensions);
+    this.setState({ box: dimensions });
+  }
 
-    onInputChange = (event) => {
-            this.setState({ input: event.target.value });
-    }
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+  }
 
-    onButtonSubmit = () => {
-        this.setState({imageUrl: this.state.input});
-        const reqOptions = returnClarifaiRequestOptions(this.state.input);
-        fetch("https://api.clarifai.com/v2/models/" + 'face-detection' + "/outputs", reqOptions)
-            .then(response => response.json())
-            .then(response => this.drawFaceBox(this.calculateFaceDimensions(response)))
-            .catch(error => console.log('error', error));
-    }
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input });
+    const reqOptions = returnClarifaiRequestOptions(this.state.input);
+    fetch("https://api.clarifai.com/v2/models/" + 'face-detection' + "/outputs", reqOptions)
+      .then(response => response.json())
+      .then(response => this.drawFaceBox(this.calculateFaceDimensions(response)))
+      .catch(error => console.log('error', error));
+  }
 
-    render() {
-        return (
-            <div className="App">
+  render() {
+    return (
+      <div className="App">
                 <ParticlesBg color="#FFFFFF" num={80} type="cobweb" bg={true} />
                 <Navigation />
                 <Logo />
                 <Rank />
                 <ImageLinkURL onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
-                <FacePattern imageUrl={this.state.imageUrl} />
+                <FacePattern box={this.state.box} imageUrl={this.state.imageUrl} />
             </div>
-        );
-    }
+    );
+  }
 }
 
 export default App;
--- section24/smart-brain-front-end/src/components/FacePattern/FacePattern.js
+++ section24/smart-brain-front-end/src/components/FacePattern/FacePattern.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import './FacePattern.css';
 
-const FacePattern = ({imageUrl}) => {
+const FacePattern = ({ imageUrl, box }) => {
   return (
-      <div className='center ma'>
+    <div className='center ma'>
         <div className='absolute mt2'>
           <img width='500px' height='auto' alt='' id='input-image' src={imageUrl}/>
+          <div className='bounding-box'></div>
         </div>
       </div>
-    )
+  )
 }
 
 export default FacePattern;
