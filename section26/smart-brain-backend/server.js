b555adf15a716ac4b18ad25e17c58c2aa48cc505
David <daveman@gmail.com>
2023-05-30 22:22:29 -0700

    completed image and profile api endpoints

--- section26/smart-brain-backend/server.js
+++ section26/smart-brain-backend/server.js
@@ -6,8 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 
 const database = {
-  users: [
-    {
+  users: [{
       id: '123',
       name: 'John',
       email: 'john@gmail.com',
@@ -27,20 +26,20 @@ const database = {
 }
 
 
-app.get('/', (req, res)=> {
+app.get('/', (req, res) => {
   res.send(database.users);
 })
 
-app.post('/signin', (req, res)=> {
+app.post('/signin', (req, res) => {
   if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
     res.json('success');
   } else {
     res.status(400).json('error logging in');
-  } 
+  }
 })
 
 app.post('/register', (req, res) => {
-  const {email, name, password} = req.body;
+  const { email, name, password } = req.body;
   database.users.push({
     id: '125',
     name: name,
@@ -49,9 +48,38 @@ app.post('/register', (req, res) => {
     entries: 0,
     joined: new Date()
   })
-  res.json(database.users[database.users.length-1]);
+  res.json(database.users[database.users.length - 1]);
 })
 
-app.listen(3000, ()=> {
+app.get('/users/:id', (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  })
+  if (!found) {
+    res.status(400).json('user not found');
+  }
+})
+
+app.put('/image', (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  })
+  if (!found) {
+    res.status(400).json('user not found');
+  }
+})
+
+app.listen(3000, () => {
   console.log('app is running on port 3000');
 })
