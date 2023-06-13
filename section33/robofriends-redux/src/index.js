7756be3011c71fbe79572821ce705b59a5bef5c2
David <daveman@gmail.com>
2023-06-12 21:10:20 -0700

    npm install redux thunk

--- section33/robofriends-redux/src/index.js
+++ section33/robofriends-redux/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore} from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 import { createRoot } from "react-dom/client";
 import './index.css';
 import App from './containers/App';
 import 'tachyons';
 import { searchRobots } from './reducers';
 
-const store = createStore(searchRobots);
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware(logger));
 
 const root = createRoot(document.getElementById('root'));
 root.render(<Provider store={store}>
