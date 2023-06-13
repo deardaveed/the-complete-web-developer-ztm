602be6989ee0f1c9f45fc5caa723d08ad4bde312
David <daveman@gmail.com>
2023-06-12 21:52:26 -0700

    finished redux conversion

--- section33/robofriends-redux/src/index.js
+++ section33/robofriends-redux/src/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { createLogger } from 'redux-logger';
 import { createRoot } from "react-dom/client";
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import 'tachyons';
-import { searchRobots } from './reducers';
+import { searchRobots, requestRobots } from './reducers';
 
 const logger = createLogger();
-const store = createStore(searchRobots, applyMiddleware(logger));
+
+const rootReducer = combineReducers({ searchRobots, requestRobots })
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
 
 const root = createRoot(document.getElementById('root'));
 root.render(<Provider store={store}>
               <App />
             </Provider>);
-
-
- 
